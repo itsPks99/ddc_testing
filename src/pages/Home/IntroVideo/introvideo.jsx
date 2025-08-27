c172c8ec7aa5c917feef494a2ee06b831c012526
DelhiDigital <delhidigitalco@gmail.com>
2025-08-27 11:22:00 +0530

    vi

--- src/pages/Home/IntroVideo/introvideo.jsx
+++ src/pages/Home/IntroVideo/introvideo.jsx
@@ -5,8 +5,8 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react"
 import "./introvideo.css"
 
 export default function IntroVideo({
-  desktopVideoSrc = "/assets/DdcWebsiteVideo-desktop.mp4",
-  mobileVideoSrc = "/assets/DdcWebsiteVideo-mobile.mp4",
+  desktopVideoSrc = "/DdcWebsiteVideo-desktop.mp4",
+  mobileVideoSrc = "/DdcWebsiteVideo-mobile.mp4",
   posterImage = "",
   autoPlay = true,
   muted = true,
